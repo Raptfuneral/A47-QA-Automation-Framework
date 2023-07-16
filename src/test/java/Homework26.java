0ddc2313e23cffa998add6d78fa151779882d44b
Angel Ayala <angel.ayala@testpro.io>
2023-07-15 19:24:42 -0700

    Update Homework26.java

--- src/test/java/Homework26.java
+++ src/test/java/Homework26.java
@@ -12,7 +12,7 @@ public class Homework26 extends BaseTest{
     HomePage homePage = new HomePage(getDriver());
 
     loginPage.enterEmail(email).enterPassword(password).clickSubmit();
-    homePage.selectPlaylist().selectEdit().editName("newName1");
+    homePage.selectPlaylist().selectEdit().editName("newName17");
 
     Assert.assertTrue(homePage.verificationMessage().contains(message));
 }
